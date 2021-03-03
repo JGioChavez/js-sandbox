3184edeafe48f15dccc06c31e6ca5baeaf797257
JChavez298 <Jchavez298@west-mec.org>
2021-03-03 08:46:28 -0700

    Completed teleport button

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-let colorSelector = document.getElementById("colorSelector"); 
+let colorSelector = document.getElementById("colorSelector");
 colorSelector.onchange = function () {
     console.log(colorSelector.value);
     document.getElementById("colorBox").style = `background-color: ${colorSelector.value};`;
 }
 
-function toggleDarkMode(){
+function toggleDarkMode() {
     let content = document.getElementById("content")
     content.classList.toggle("dark-mode")
 }
 
-function teleport(){
-    console.log("teleport")
+function teleport(distance) {
     let runaway = document.getElementById("runaway")
+    console.log(runaway.style.top, " - ", runaway.style.left);
+    if (runaway.style.top == 0 && runaway.style.left == 0) {
+        runaway.style.top = `${distance}px`;
+    }
+    else if (runaway.style.top == `${distance}px` && runaway.style.left == 0) {
+        runaway.style.left = `${distance}px`;
+    }
+    else if (runaway.style.top == `${distance}px` && runaway.style.left == `${distance}px`) {
+        runaway.style.top = "0px"
+    }
+    else if (runaway.style.top == "0px" && runaway.style.left == `${distance}px`) {
+        runaway.style.left = null;
+        runaway.style.top = null;
+    }
+    //runaway.style.top  = `${distance}px`;
     runaway.innerText = "Something else"
 }
